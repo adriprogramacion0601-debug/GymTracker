96f8d7f4794733185cfa264df4bec7e0ba083063
Adrián Domínguez Obrero <adriprogramacion0601@gmail.com>
2026-02-10 21:43:28 +0100

    casi completo JWT

--- GymTracker/settings.py
+++ GymTracker/settings.py
@@ -12,6 +12,15 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated'
+    ),
+}
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
--- entrenamientos/views.py
+++ entrenamientos/views.py
@@ -1,12 +1,16 @@
 from rest_framework import viewsets
 from .models import Rutina, Ejercicio
 from .serializers import RutinaSerializer, EjercicioSerializer
-
+from rest_framework.permissions import IsAuthenticated
 
 class RutinaViewSet(viewsets.ModelViewSet):
     queryset = Rutina.objects.all()
     serializer_class = RutinaSerializer
+    permission_classes = [IsAuthenticated]
 
 class EjercicioViewSet(viewsets.ModelViewSet):
     queryset = Ejercicio.objects.all()
     serializer_class = EjercicioSerializer
+    permission_classes = [IsAuthenticated]
+
+    
